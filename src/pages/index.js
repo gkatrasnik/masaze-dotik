018ca11a0caac9de1438694dd2d88ef80f6fb294
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-10-02 13:37:41 +0200

    prva kontakt na vrhu

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,27 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Domov" />
     <h1 className="text-center mb-8">Masaže Dotik</h1>
+
     <div className="flex flex-wrap-reverse justify-center md:flex-nowrap bg-gray-100 border-4 py-4 md:py-8 px-4 m-2 rounded-lg">
-      <p className="p-4 md:w-1/2 ">
-        Sem Jernej Švab, maser in terapevt. Pri svojem delu skupaj s stranko
-        najprej opredeliva težavo, nato pa s celostnim pristopom poiščemo vzrok
-        problema in ga odpravimo. Pri delu uporabljam različne tehnike in
-        izkušnje pridobljene z leti dela in izobraževanj.
-      </p>
-      <StaticImage
-        src="../images/jernej_svab_masaze_dotik_2.jpeg"
-        alt="Jernej Švab, Masaže Dotik"
-        width={400}
-        className="rounded-lg"
-      />
-    </div>
-    <div className="flex flex-wrap justify-center md:flex-nowrap bg-gray-100 border-4 py-4 md:py-8 px-4 m-2 rounded-lg">
-      <StaticImage
-        src="../images/pri-delu.jpg"
-        alt="Jernej Švab, Masaže Dotik"
-        width={400}
-        className="rounded-lg"
-      />
       <div className="p-4 md:w-1/2 ">
         <div className="relative md:top-2 lg:top-8">
           <p className="text-lg">
@@ -52,6 +33,26 @@ const IndexPage = () => (
           </p>
         </div>
       </div>
+      <StaticImage
+        src="../images/jernej_svab_masaze_dotik_2.jpeg"
+        alt="Jernej Švab, Masaže Dotik"
+        width={400}
+        className="rounded-lg"
+      />
+    </div>
+    <div className="flex flex-wrap justify-center md:flex-nowrap bg-gray-100 border-4 py-4 md:py-8 px-4 m-2 rounded-lg">
+      <StaticImage
+        src="../images/pri-delu.jpg"
+        alt="Jernej Švab, Masaže Dotik"
+        width={400}
+        className="rounded-lg"
+      />
+      <p className="p-4 md:w-1/2 ">
+        Sem Jernej Švab, maser in terapevt. Pri svojem delu skupaj s stranko
+        najprej opredeliva težavo, nato pa s celostnim pristopom poiščemo vzrok
+        problema in ga odpravimo. Pri delu uporabljam različne tehnike in
+        izkušnje pridobljene z leti dela in izobraževanj.
+      </p>
     </div>
   </Layout>
 )
